db52c20e7189d4ea7913398a4120d60412439b9c
Ross MacLeod <ross@confer.care>
2017-03-06 22:18:02 -0500

    doc comments

--- composite-opaleye/src/Composite/Opaleye/Util.hs
+++ composite-opaleye/src/Composite/Opaleye/Util.hs
@@ -3,6 +3,8 @@ module Composite.Opaleye.Util where
 import Data.Profunctor (dimap)
 import Opaleye (Column, Constant, unsafeCoerceColumn)
 
+-- |Coerce one type of 'Column' 'Constant' profunctor to another using by just asserting the changed type on the column side and using the given function
+-- on the Haskell side. Useful when the PG value representation is the same but the Haskell type changes, e.g. for enums.
 constantColumnUsing :: Constant haskell (Column pgType)
                     -> (haskell' -> haskell)
                     -> Constant haskell' (Column pgType')
