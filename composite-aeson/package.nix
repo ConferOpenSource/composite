7f893bf5f23534073c5546117a9d48be298301a9
Ross MacLeod <ross@confer.care>
2017-03-21 16:41:59 -0400

    update nix build artifacts

--- composite-aeson/package.nix
+++ composite-aeson/package.nix
@@ -1,7 +1,8 @@
-{ mkDerivation, aeson, aeson-better-errors, base, basic-prelude
-, composite-base, containers, contravariant, generic-deriving, lens
-, profunctors, scientific, stdenv, text, unordered-containers
-, vinyl
+{ mkDerivation, aeson, aeson-better-errors, aeson-qq, base
+, basic-prelude, composite-base, containers, contravariant, Frames
+, generic-deriving, hspec, lens, profunctors, QuickCheck
+, scientific, stdenv, tagged, template-haskell, text, time
+, unordered-containers, vector, vinyl
 }:
 mkDerivation {
   pname = "composite-aeson";
@@ -9,8 +10,15 @@ mkDerivation {
   src = ./.;
   libraryHaskellDepends = [
     aeson aeson-better-errors base basic-prelude composite-base
-    containers contravariant generic-deriving lens profunctors
-    scientific text unordered-containers vinyl
+    containers contravariant Frames generic-deriving lens profunctors
+    scientific tagged template-haskell text time unordered-containers
+    vector vinyl
+  ];
+  testHaskellDepends = [
+    aeson aeson-better-errors aeson-qq base basic-prelude
+    composite-base containers contravariant Frames generic-deriving
+    hspec lens profunctors QuickCheck scientific tagged
+    template-haskell text time unordered-containers vector vinyl
   ];
   homepage = "https://github.com/ConferHealth/composite#readme";
   description = "JSON for Vinyl/Frames records";
