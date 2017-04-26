363d78f154fafabe42b7df4d2ea12484048398ab
Asa Riley <asa@confer.care>
2017-04-26 17:40:00 -0400

    bump to v0.3.1.0

--- composite-base/package.nix
+++ composite-base/package.nix
@@ -1,18 +1,18 @@
-{ mkDerivation, base, hspec, lens, monad-control, mtl, QuickCheck
-, stdenv, template-haskell, text, transformers, transformers-base
-, vinyl
+{ mkDerivation, base, exceptions, hspec, lens, monad-control, mtl
+, QuickCheck, stdenv, template-haskell, text, transformers
+, transformers-base, vinyl
 }:
 mkDerivation {
   pname = "composite-base";
-  version = "0.3.0.0";
+  version = "0.3.1.0";
   src = ./.;
   libraryHaskellDepends = [
-    base lens monad-control mtl template-haskell text transformers
-    transformers-base vinyl
+    base exceptions lens monad-control mtl template-haskell text
+    transformers transformers-base vinyl
   ];
   testHaskellDepends = [
-    base hspec lens monad-control mtl QuickCheck template-haskell text
-    transformers transformers-base vinyl
+    base exceptions hspec lens monad-control mtl QuickCheck
+    template-haskell text transformers transformers-base vinyl
   ];
   homepage = "https://github.com/ConferHealth/composite#readme";
   description = "Shared utilities for composite-* packages";
