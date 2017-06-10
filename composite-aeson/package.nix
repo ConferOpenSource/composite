cf7e36ae4ed12c0787ddd76eabfdcf7dcd8a62bc
Ross MacLeod <ross@confer.care>
2017-06-09 23:37:12 -0400

    add wrapJsonFormat to module exports and add jsonFormatWithoutCustomError

--- composite-aeson/package.nix
+++ composite-aeson/package.nix
@@ -1,7 +1,7 @@
 { mkDerivation, aeson, aeson-better-errors, aeson-qq, base
 , composite-base, containers, contravariant, generic-deriving
-, hashable, hspec, lens, profunctors, QuickCheck, scientific
-, stdenv, tagged, template-haskell, text, time
+, hashable, hspec, lens, mmorph, mtl, profunctors, QuickCheck
+, scientific, stdenv, tagged, template-haskell, text, time
 , unordered-containers, vector, vinyl
 }:
 mkDerivation {
@@ -10,13 +10,14 @@ mkDerivation {
   src = ./.;
   libraryHaskellDepends = [
     aeson aeson-better-errors base composite-base containers
-    contravariant generic-deriving hashable lens profunctors scientific
-    tagged template-haskell text time unordered-containers vector vinyl
+    contravariant generic-deriving hashable lens mmorph mtl profunctors
+    scientific tagged template-haskell text time unordered-containers
+    vector vinyl
   ];
   testHaskellDepends = [
     aeson aeson-better-errors aeson-qq base composite-base containers
-    contravariant generic-deriving hashable hspec lens profunctors
-    QuickCheck scientific tagged template-haskell text time
+    contravariant generic-deriving hashable hspec lens mmorph mtl
+    profunctors QuickCheck scientific tagged template-haskell text time
     unordered-containers vector vinyl
   ];
   homepage = "https://github.com/ConferHealth/composite#readme";
