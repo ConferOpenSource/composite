07b9da897d1be2ade732432dd7955160e34ed7c0
Ross MacLeod <ross@confer.care>
2017-09-13 15:02:05 -0400

    fix bug where enumJsonFormat would encode/decode to the wrong strings for ADTs larger than 3
    thanks, generic-deriving.

--- composite-aeson/test/EnumSpec.hs
+++ composite-aeson/test/EnumSpec.hs
@@ -4,19 +4,35 @@ module EnumSpec where
 import Data.Void (Void)
 import Data.Aeson (Value(String))
 import GHC.Generics (Generic)
-import Composite.Aeson.Base (JsonFormat, fromJsonWithFormat)
+import Composite.Aeson.Base (JsonFormat, fromJsonWithFormat, toJsonWithFormat)
 import Composite.Aeson.Enum (enumJsonFormat)
+import Composite.Aeson.Formats.Provided (listJsonFormat)
 import qualified Data.Aeson.BetterErrors as ABE
+import Data.Aeson.QQ (aesonQQ)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 data TestEnum = A | B deriving (Show, Eq, Ord, Generic)
 
+data LargerTestEnum = D | E | F | G deriving (Show, Eq, Ord, Generic)
+
 testEnumFormat :: JsonFormat Void TestEnum
 testEnumFormat = enumJsonFormat ""
 
+largerTestEnumFormat :: JsonFormat Void LargerTestEnum
+largerTestEnumFormat = enumJsonFormat ""
+
 enumSuite :: Spec
 enumSuite =
-  describe "enumJsonFormat" $
+  describe "enumJsonFormat" $ do
+    describe "when mapping ADTs with more than two branches" $ do
+      it "should encode each value correctly" $
+        toJsonWithFormat (listJsonFormat largerTestEnumFormat) [D,E,F,G]
+          `shouldBe` [aesonQQ|["D", "E", "F", "G"]|]
+
+      it "should decode each value correctly" $
+        ABE.parseValue (fromJsonWithFormat (listJsonFormat largerTestEnumFormat)) [aesonQQ|["D", "E", "F", "G"]|]
+          `shouldBe` Right [D,E,F,G]
+
     describe "when input value does not match any of enum constructors" $
       it "should return a parse error, not throw an exception" $
         ABE.parseValue (fromJsonWithFormat testEnumFormat) (String "C")
