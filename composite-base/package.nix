f0532dc96b49544219f0402553e94c1abe4723d0
Ross MacLeod <ross@vitalbio.com>
2020-07-20 14:17:50 -0400

    bump to v0.7.2.0

--- composite-base/package.nix
+++ composite-base/package.nix
@@ -4,7 +4,7 @@
 }:
 mkDerivation {
   pname = "composite-base";
-  version = "0.7.1.0";
+  version = "0.7.2.0";
   src = ./.;
   libraryHaskellDepends = [
     base exceptions lens monad-control mtl profunctors template-haskell
--- composite-ekg/package.nix
+++ composite-ekg/package.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "composite-ekg";
-  version = "0.7.1.0";
+  version = "0.7.2.0";
   src = ./.;
   libraryHaskellDepends = [
     base composite-base ekg-core lens text vinyl
