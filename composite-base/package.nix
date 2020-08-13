5f5feb447e0ee4df65a4f4b0182fef4f8c129510
Dan Firth <locallycompact@gmail.com>
2020-08-13 16:19:07 +0100

    Add NFData instances.

--- composite-base/package.nix
+++ composite-base/package.nix
@@ -1,20 +1,22 @@
-{ mkDerivation, base, exceptions, hpack, hspec, lens, monad-control
-, mtl, profunctors, QuickCheck, stdenv, template-haskell, text
-, transformers, transformers-base, unliftio-core, vinyl
+{ mkDerivation, base, deepseq, exceptions, hpack, hspec, lens
+, monad-control, mtl, profunctors, QuickCheck, stdenv
+, template-haskell, text, transformers, transformers-base
+, unliftio-core, vinyl
 }:
 mkDerivation {
   pname = "composite-base";
   version = "0.7.3.0";
   src = ./.;
   libraryHaskellDepends = [
-    base exceptions lens monad-control mtl profunctors template-haskell
-    text transformers transformers-base unliftio-core vinyl
+    base deepseq exceptions lens monad-control mtl profunctors
+    template-haskell text transformers transformers-base unliftio-core
+    vinyl
   ];
   libraryToolDepends = [ hpack ];
   testHaskellDepends = [
-    base exceptions hspec lens monad-control mtl profunctors QuickCheck
-    template-haskell text transformers transformers-base unliftio-core
-    vinyl
+    base deepseq exceptions hspec lens monad-control mtl profunctors
+    QuickCheck template-haskell text transformers transformers-base
+    unliftio-core vinyl
   ];
   prePatch = "hpack";
   homepage = "https://github.com/ConferOpenSource/composite#readme";
