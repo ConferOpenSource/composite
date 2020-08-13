9fb68389dc2db35c9a2cd57aa22c947ae862b61e
Dan Firth <locallycompact@gmail.com>
2020-08-13 19:57:48 +0100

    Add composite-aeson-path

---
+++ composite-aeson-path/src/Composite/Aeson/Path.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Composite.Aeson.Path where
+
+import Composite.Aeson(JsonFormat, aesonJsonFormat)
+import Path(Path, Rel, Abs, File, Dir)
+
+relFileJsonFormat :: JsonFormat e (Path Rel File)
+relFileJsonFormat = aesonJsonFormat
+
+relDirJsonFormat :: JsonFormat e (Path Rel Dir)
+relDirJsonFormat = aesonJsonFormat
+
+absFileJsonFormat :: JsonFormat e (Path Abs File)
+absFileJsonFormat = aesonJsonFormat
+
+absDirJsonFormat :: JsonFormat e (Path Abs Dir)
+absDirJsonFormat = aesonJsonFormat
