933b50a6e5e3508225bd4f116fb7c477e21b8d36
Dan Firth <locallycompact@gmail.com>
2020-08-16 10:21:22 +0100

    Add DefaultJsonFormat instances

--- composite-aeson-path/src/Composite/Aeson/Path.hs
+++ composite-aeson-path/src/Composite/Aeson/Path.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Composite.Aeson.Path where
 
-import Composite.Aeson(JsonFormat, aesonJsonFormat)
+import Composite.Aeson(JsonFormat, aesonJsonFormat, DefaultJsonFormat(defaultJsonFormat))
 import Path(Path, Rel, Abs, File, Dir)
 
 relFileJsonFormat :: JsonFormat e (Path Rel File)
@@ -15,3 +15,15 @@ absFileJsonFormat = aesonJsonFormat
 
 absDirJsonFormat :: JsonFormat e (Path Abs Dir)
 absDirJsonFormat = aesonJsonFormat
+
+instance DefaultJsonFormat (Path Rel File) where
+  defaultJsonFormat = relFileJsonFormat
+
+instance DefaultJsonFormat (Path Rel Dir) where
+  defaultJsonFormat = relDirJsonFormat
+
+instance DefaultJsonFormat (Path Abs File) where
+  defaultJsonFormat = absFileJsonFormat
+
+instance DefaultJsonFormat (Path Abs Dir) where
+  defaultJsonFormat = absDirJsonFormat
